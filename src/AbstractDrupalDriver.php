d0ea37e24c50bce8aafb5b1106fe5482a6d66144
mccrodp <info@opensourceopenmind.ninja>
2016-06-30 10:17:38 +0100

    Initial Driver port to Drupal.

--- src/AbstractDrupalDriver.php
+++ src/AbstractDrupalDriver.php
@@ -1,87 +1,81 @@
 <?php
-//
-///**
-// * @file
-// * Message FOS Abstract Drupal Driver.
-// */
-//
-//namespace Drupal\message_fos;
-//
-//use Doctrine\Common\Persistence\ObjectManager;
-//use FOS\Message\Driver\AbstractDriver;
-//use FOS\Message\Model\ConversationInterface;
-//use FOS\Message\Model\ConversationPersonInterface;
-//use FOS\Message\Model\MessageInterface;
-//use FOS\Message\Model\MessagePersonInterface;
-//
-///**
-// * Abstract driver for Drupal entity manager.
-// */
-//abstract class AbstractDrupalDriver extends AbstractDriver
-//{
-//    /**
-//     * @var ObjectManager|EntityManager
-//     */
-//    protected $objectManager;
-//
-//    /**
-//     * Constructor.
-//     *
-//     * @param ObjectManager $objectManager
-//     * @param string        $conversationClass
-//     * @param string        $conversationPersonClass
-//     * @param string        $messageClass
-//     * @param string        $messagePersonClass
-//     */
-//    public function __construct(
-//        ObjectManager $objectManager,
-//        $conversationClass,
-//        $conversationPersonClass,
-//        $messageClass,
-//        $messagePersonClass
-//    ) {
-//        parent::__construct($conversationClass, $conversationPersonClass, $messageClass, $messagePersonClass);
-//
-//        $this->objectManager = $objectManager;
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function persistConversation(ConversationInterface $conversation)
-//    {
-//        $this->objectManager->persist($conversation);
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function persistConversationPerson(ConversationPersonInterface $conversationPerson)
-//    {
-//        $this->objectManager->persist($conversationPerson);
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function persistMessage(MessageInterface $message)
-//    {
-//        $this->objectManager->persist($message);
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function persistMessagePerson(MessagePersonInterface $messagePerson)
-//    {
-//        $this->objectManager->persist($messagePerson);
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function flush()
-//    {
-//        $this->objectManager->flush();
-//    }
-//}
+
+/**
+ * @file
+ * Message FOS Abstract Drupal Driver.
+ */
+
+namespace Drupal\message_fos;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use FOS\Message\Driver\AbstractDriver;
+use FOS\Message\Model\ConversationInterface;
+use FOS\Message\Model\ConversationPersonInterface;
+use FOS\Message\Model\MessageInterface;
+use FOS\Message\Model\MessagePersonInterface;
+
+/**
+ * Abstract driver for Drupal entity manager.
+ */
+abstract class AbstractDrupalDriver extends AbstractDriver {
+  /**
+   * @var EntityTypeManager
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructor.
+   *
+   * @param EntityTypeManager $entityTypeManager
+   * @param string $conversationClass
+   * @param string $conversationPersonClass
+   * @param string $messageClass
+   * @param string $messagePersonClass
+   */
+  public function __construct(
+    EntityTypeManager $entityTypeManager,
+    $conversationClass,
+    $conversationPersonClass,
+    $messageClass,
+    $messagePersonClass
+  ) {
+    parent::__construct($conversationClass, $conversationPersonClass, $messageClass, $messagePersonClass);
+
+    $this->entityTypeManager = $entityTypeManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function persistConversation(ConversationInterface $conversation) {
+    $this->entityTypeManager->getStorage('conversation')->create($conversation);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function persistConversationPerson(ConversationPersonInterface $conversationPerson) {
+    $this->entityTypeManager->getStorage('conversation_person')->create($conversationPerson);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function persistMessage(MessageInterface $message) {
+    $this->entityTypeManager->getStorage('message')->create($message);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function persistMessagePerson(MessagePersonInterface $messagePerson) {
+    $this->entityTypeManager->getStorage('message_person')->create($messagePerson);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function flush() {
+    // @todo what is the persistence layer doing here and do we need a Drupal equivalent?
+  }
+}
--- src/DrupalDriver.php
+++ src/DrupalDriver.php
@@ -1,158 +1,154 @@
 <?php
+/**
+ * @file
+ * Message FOS Drupal Driver.
+ */
+
+namespace Drupal\message_fos;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use FOS\Message\Model\ConversationInterface;
+use FOS\Message\Model\PersonInterface;
+use FOS\Message\Model\TagInterface;
+
+/**
+ * Driver for Drupal.
+ */
+class DrupalDriver extends AbstractDrupalDriver {
+  /**
+   * Constructor.
+   *
+   * @param EntityTypeManager $entityTypeManager
+   * @param string $conversationClass
+   * @param string $conversationPersonClass
+   * @param string $messageClass
+   * @param string $messagePersonClass
+   */
+  public function __construct(
+    EntityTypeManager $entityTypeManager,
+    $conversationClass = 'Drupal\message_private\Entity\Conversation',
+    $conversationPersonClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\ConversationPerson',
+    $messageClass = 'Drupal\message\Entity\Message',
+    $messagePersonClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\MessagePerson'
+  ) {
+    parent::__construct(
+      $entityTypeManager,
+      $conversationClass,
+      $conversationPersonClass,
+      $messageClass,
+      $messagePersonClass
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function findPersonConversations(PersonInterface $person, TagInterface $tag = NULL) {
+//    $qb = $this->createConversationQueryBuilder();
 //
-///**
-// * @file
-// * Message FOS Drupal Driver.
-// */
+//    // Last message date order
+//    $lastMessageDateSubquery = $this->entityTypeManager->createQueryBuilder()
+//      ->select('MAX(lmds.read)')
+//      ->from($this->getMessagePersonClass(), 'lmds')
+//      ->leftJoin('lmds.message', 'lmdsm')
+//      ->where('lmds.person = :person')
+//      ->andWhere('IDENTITY(lmdsm.conversation) = c.id')
+//      ->getDQL();
 //
-//namespace Drupal\message_fos;
-//use FOS\Message\Model\PersonInterface;
-//use FOS\Message\Model\TagInterface;
+//    $qb->addSelect('(' . $lastMessageDateSubquery . ') AS HIDDEN lastMessageDate');
+//    $qb->orderBy('lastMessageDate', 'DESC');
 //
-///**
-// * Driver for Drupal.
-// */
-//class DrupalDriver extends AbstractDrupalDriver
-//{
-//    /**
-//     * Constructor.
-//     *
-//     * @param EntityManager $objectManager
-//     * @param string        $conversationClass
-//     * @param string        $conversationPersonClass
-//     * @param string        $messageClass
-//     * @param string        $messagePersonClass
-//     */
-//    public function __construct(
-//        EntityManager $objectManager,
-//        $conversationClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\Conversation',
-//        $conversationPersonClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\ConversationPerson',
-//        $messageClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\Message',
-//        $messagePersonClass = 'FOS\Message\Driver\Doctrine\ORM\Entity\MessagePerson'
-//    ) {
-//        parent::__construct(
-//            $objectManager,
-//            $conversationClass,
-//            $conversationPersonClass,
-//            $messageClass,
-//            $messagePersonClass
-//        );
+//    // Person filter
+//    $personSubquery = $this->entityTypeManager->createQueryBuilder()
+//      ->select('IDENTITY(pfcp.conversation)')
+//      ->from($this->getConversationPersonClass(), 'pfcp')
+//      ->where('pfcp.person = :person')
+//      ->getDQL();
+//
+//    $qb->where($qb->expr()->in('c.id', $personSubquery));
+//    $qb->setParameter('person', $person->getId());
+//
+//    // Tag filter
+//    if ($tag !== NULL) {
+//      $tag = $tag->getName();
+//
+//      $tagSubquerey = $this->objectManager->createQueryBuilder()
+//        ->select('IDENTITY(tfcp.conversation)')
+//        ->from($this->getConversationPersonClass(), 'tfcp')
+//        ->leftJoin('tfcp.tags', 'tft')
+//        ->where('tft.name = :tag')
+//        ->getDQL();
+//
+//      $qb->andWhere($qb->expr()->in('c.id', $tagSubquerey));
+//      $qb->setParameter('tag', $tag);
 //    }
 //
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function findPersonConversations(PersonInterface $person, TagInterface $tag = null)
-//    {
-//        $qb = $this->createConversationQueryBuilder();
+//    return new ArrayCollection($qb->getQuery()->getResult());
+  }
+
+  /**
+   * Create a conversation query builder with optimized joins.
+   *
+   * @return \Doctrine\ORM\QueryBuilder
+   */
+  private function createConversationQueryBuilder() {
+//    return $this->objectManager->createQueryBuilder()
+//      ->select('c', 'cp', 'p', 't', 'm', 'mp')
+//      ->from($this->getConversationClass(), 'c')
+//      ->leftJoin('c.persons', 'cp')
+//      ->leftJoin('cp.person', 'p')
+//      ->leftJoin('cp.tags', 't')
+//      ->leftJoin('c.messages', 'm')
+//      ->leftJoin('m.persons', 'mp')
+//      ->orderBy('m.date', 'ASC')
+//      ->addOrderBy('m.id', 'ASC');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function findConversationPerson(ConversationInterface $conversation, PersonInterface $person) {
+//    return $this->objectManager->createQueryBuilder()
+//      ->select('cp', 'c', 'p', 't')
+//      ->from($this->getConversationPersonClass(), 'cp')
+//      ->innerJoin('cp.conversation', 'c')
+//      ->innerJoin('cp.person', 'p')
+//      ->leftJoin('cp.tags', 't')
+//      ->where('c.id = :conversation')
+//      ->setParameter('conversation', $conversation->getId())
+//      ->andWhere('p.id = :person')
+//      ->setParameter('person', $person->getId())
+//      ->setMaxResults(1)
+//      ->getQuery()
+//      ->getOneOrNullResult();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function findConversation($id) {
+//    return $this->createConversationQueryBuilder()
+//      ->where('c.id = :id')
+//      ->setParameter('id', $id)
+//      ->getQuery()
+//      ->getOneOrNullResult();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function findMessages(ConversationInterface $conversation, $offset = 0, $limit = 20, $sortDirection = 'ASC') {
+//    $qb = $this->objectManager->createQueryBuilder()
+//      ->select('m', 's')
+//      ->from($this->getMessageClass(), 'm')
+//      ->leftJoin('m.sender', 's')
+//      ->where('m.conversation = :conversation')
+//      ->setParameter('conversation', $conversation->getId())
+//      ->setMaxResults($limit)
+//      ->setFirstResult($offset)
+//      ->orderBy('m.date', $sortDirection)
+//      ->addOrderBy('m.id', $sortDirection);
 //
-//        // Last message date order
-//        $lastMessageDateSubquery = $this->objectManager->createQueryBuilder()
-//            ->select('MAX(lmds.read)')
-//            ->from($this->getMessagePersonClass(), 'lmds')
-//            ->leftJoin('lmds.message', 'lmdsm')
-//            ->where('lmds.person = :person')
-//            ->andWhere('IDENTITY(lmdsm.conversation) = c.id')
-//            ->getDQL();
-//
-//        $qb->addSelect('('. $lastMessageDateSubquery .') AS HIDDEN lastMessageDate');
-//        $qb->orderBy('lastMessageDate', 'DESC');
-//
-//        // Person filter
-//        $personSubquery = $this->objectManager->createQueryBuilder()
-//            ->select('IDENTITY(pfcp.conversation)')
-//            ->from($this->getConversationPersonClass(), 'pfcp')
-//            ->where('pfcp.person = :person')
-//            ->getDQL();
-//
-//        $qb->where($qb->expr()->in('c.id', $personSubquery));
-//        $qb->setParameter('person', $person->getId());
-//
-//        // Tag filter
-//        if ($tag !== null) {
-//            $tag = $tag->getName();
-//
-//            $tagSubquerey = $this->objectManager->createQueryBuilder()
-//                ->select('IDENTITY(tfcp.conversation)')
-//                ->from($this->getConversationPersonClass(), 'tfcp')
-//                ->leftJoin('tfcp.tags', 'tft')
-//                ->where('tft.name = :tag')
-//                ->getDQL();
-//
-//            $qb->andWhere($qb->expr()->in('c.id', $tagSubquerey));
-//            $qb->setParameter('tag', $tag);
-//        }
-//
-//        return new ArrayCollection($qb->getQuery()->getResult());
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function findConversationPerson(ConversationInterface $conversation, PersonInterface $person)
-//    {
-//        return $this->objectManager->createQueryBuilder()
-//            ->select('cp', 'c', 'p', 't')
-//            ->from($this->getConversationPersonClass(), 'cp')
-//            ->innerJoin('cp.conversation', 'c')
-//            ->innerJoin('cp.person', 'p')
-//            ->leftJoin('cp.tags', 't')
-//            ->where('c.id = :conversation')
-//            ->setParameter('conversation', $conversation->getId())
-//            ->andWhere('p.id = :person')
-//            ->setParameter('person', $person->getId())
-//            ->setMaxResults(1)
-//            ->getQuery()
-//            ->getOneOrNullResult();
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function findConversation($id)
-//    {
-//        return $this->createConversationQueryBuilder()
-//            ->where('c.id = :id')
-//            ->setParameter('id', $id)
-//            ->getQuery()
-//            ->getOneOrNullResult();
-//    }
-//
-//    /**
-//     * {@inheritdoc}
-//     */
-//    public function findMessages(ConversationInterface $conversation, $offset = 0, $limit = 20, $sortDirection = 'ASC')
-//    {
-//        $qb = $this->objectManager->createQueryBuilder()
-//            ->select('m', 's')
-//            ->from($this->getMessageClass(), 'm')
-//            ->leftJoin('m.sender', 's')
-//            ->where('m.conversation = :conversation')
-//            ->setParameter('conversation', $conversation->getId())
-//            ->setMaxResults($limit)
-//            ->setFirstResult($offset)
-//            ->orderBy('m.date', $sortDirection)
-//            ->addOrderBy('m.id', $sortDirection);
-//
-//        return new ArrayCollection($qb->getQuery()->getResult());
-//    }
-//
-//    /**
-//     * Create a conversation query builder with optimized joins.
-//     *
-//     * @return \Doctrine\ORM\QueryBuilder
-//     */
-//    private function createConversationQueryBuilder()
-//    {
-//        return $this->objectManager->createQueryBuilder()
-//            ->select('c', 'cp', 'p', 't', 'm', 'mp')
-//            ->from($this->getConversationClass(), 'c')
-//            ->leftJoin('c.persons', 'cp')
-//            ->leftJoin('cp.person', 'p')
-//            ->leftJoin('cp.tags', 't')
-//            ->leftJoin('c.messages', 'm')
-//            ->leftJoin('m.persons', 'mp')
-//            ->orderBy('m.date', 'ASC')
-//            ->addOrderBy('m.id', 'ASC');
-//    }
-//}
+//    return new ArrayCollection($qb->getQuery()->getResult());
+  }
+}
